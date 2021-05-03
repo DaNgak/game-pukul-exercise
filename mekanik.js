69ab7d7188b477b606d468767eda09130f1a49f2
Aria_250602 <ariapratama.2002@gmail.com>
2021-05-03 09:52:04 +0700

    first commit

---
+++ mekanik.js
@@ -0,0 +1,60 @@
+const start = document.querySelector(".button-start");
+const sahrul = document.querySelectorAll("img");
+const rip = document.querySelectorAll(".rip");
+const score = document.querySelector(".inti-score");
+let indexrandom, indexsebelumnya, selesai, skor;
+
+function posisirandom(sahrul) {
+    indexrandom = Math.floor(Math.random() * sahrul.length);
+    if (indexrandom === indexsebelumnya) {
+        while (indexrandom === indexsebelumnya) {
+            indexrandom = Math.floor(Math.random() * sahrul.length);
+        }
+    }
+    indexsebelumnya = indexrandom;
+    return indexrandom;
+}
+
+function timerandom(mininal, maximal) {
+    return Math.round(Math.random() * (maximal - mininal) + mininal);
+}
+
+function munculkanSiSahrul() {
+    const posisi = posisirandom(sahrul);
+    const waktu = timerandom(300, 400);
+    sahrul[posisi].classList.add("munculkansahrul");
+    setTimeout(() => {
+        hilangkanSiSahrul(posisi);
+        if (!selesai) {
+            munculkanSiSahrul();
+        }
+    }, waktu);
+}
+
+function hilangkanSiSahrul(index) {
+    sahrul[index].classList.remove("munculkansahrul");
+}
+
+function keclick() {
+    skor++;
+    score.innerHTML = skor;
+    this.classList.remove("munculkansahrul");
+}
+
+sahrul.forEach((t) => {
+    t.addEventListener("click", keclick);
+});
+
+function callback() {
+    skor = 0;
+    selesai = false;
+    score.innerHTML = skor;
+    munculkanSiSahrul();
+    setTimeout(() => {
+        selesai = true;
+    }, 10000);
+}
+
+function mulai() {
+    callback();
+}
